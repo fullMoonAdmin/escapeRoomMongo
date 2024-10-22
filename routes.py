a9680d25bcfdb11e0de0c5e924eca2c3d0ba727d
Anthony Lopez <coordinator@fullmoonprod.com>
2024-10-22 11:01:09 -0500

    increase records buffer size

--- routes.py
+++ routes.py
@@ -6,6 +6,7 @@ from models import GameRecord, GameRecordUpdate
 
 router = APIRouter()
 
+
 @router.post("/", response_description="Create a new record", status_code=status.HTTP_201_CREATED, response_model=GameRecord)
 def create_records(request: Request, record: GameRecord = Body(...)):
     record = jsonable_encoder(record)
@@ -16,16 +17,20 @@ def create_records(request: Request, record: GameRecord = Body(...)):
 
     return created_record
 
+
 @router.get("/", response_description="List all records", response_model=List[GameRecord])
 def list_records(request: Request):
-    records = list(request.app.database["bankJobData"].find(limit=100))
+    records = list(request.app.database["bankJobData"].find(limit=9999))
     return records
+
 
 @router.get("/{id}", response_description="Get a single record by id", response_model=GameRecord)
 def find_record(id: str, request: Request):
     if (record := request.app.database["bankJobData"].find_one({"_id": id})) is not None:
         return record
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Record with ID {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Record with ID {id} not found")
+
 
 @router.put("/{id}", response_description="Update a record", response_model=GameRecord)
 def update_record(id: str, request: Request, record: GameRecordUpdate = Body(...)):
@@ -36,14 +41,17 @@ def update_record(id: str, request: Request, record: GameRecordUpdate = Body(...
         )
 
         if update_result.modified_count == 0:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Record with ID {id} not found")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Record with ID {id} not found")
 
     if (
         existing_record := request.app.database["bankJobData"].find_one({"_id": id})
     ) is not None:
         return existing_record
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Record with ID {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Record with ID {id} not found")
+
 
 @router.delete("/{id}", response_description="Delete a record")
 def delete_record(id: str, request: Request, response: Response):
@@ -53,4 +61,5 @@ def delete_record(id: str, request: Request, response: Response):
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Record with ID {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Record with ID {id} not found")
